f3663c0ce209fb81314154abd2765da16c4e7080
tanulo <tanulo@barossgabor.local>
2025-09-29 13:37:46 +0200

    keypress event

---
+++ 3_keypress_event/script.js
@@ -0,0 +1,16 @@
+const demo = document.querySelector('#demo');
+const area = document.querySelector('#area');
+
+// console.log(area, demo);
+
+area.addEventListener('keyup', () => {
+    // let star = '';
+
+    // for (let i = 0; i < area.value.length; i++) {
+    //     star = '*';
+    // }
+
+    // demo.textContent = star;
+
+    demo.textContent = '*'.repeat(area.value.length);
+});
