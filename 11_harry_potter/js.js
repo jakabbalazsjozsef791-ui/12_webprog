ef618a40b78c6899e83db07524e226f9dd4ec297
tanulo <tanulo@barossgabor.local>
2026-02-09 14:11:52 +0100

    11_harry_potter

--- 11_harry_potter/js.js
+++ 11_harry_potter/js.js
@@ -15,12 +15,46 @@ async function fetchCharacters() {
     } catch (error) {
         alert(error);
     }
-}
+};
 
 function displayCharacters(chars) {
     // console.log(chars);
+    if (!chars) {
+        return alert('A tömböt elfelejtetted elküldeni')
+    }
 
-    chars.forEach(chars => {
-        
+    chars.forEach(char => {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const img = document.createElement('img');
+        img.src = char.image;
+        img.alt = char.name;
+
+        const name = document.createElement('div');
+        name.className = 'name';
+        name.textContent = char.name || 'Unknown';
+
+        const house = document.createElement('div');
+        house.textContent = `Ház: ${char.house || 'Unknown'}`
+
+        const year = document.createElement('div');
+        year.textContent = `Születési év: ${char.yearOfBirth || 'Unknown'}`
+
+        const actor = document.createElement('div');
+        actor.textContent = `Színész ${char.actor || 'Unknown'}`
+
+        card.append(img, name, house, year, actor);
+
+        characters.append(card);
     });
-}
+};
+
+function fixImageUrl(url) {
+    if (!url) {
+        return ""
+    }
+
+    return url.replace('http://hp-api.herokuapp.com', 'https://hp-api.onrender.com');
+    //.replace('http://hp-api.herokuapp.com', 'https://hp-api.onrender.com'); .replace('http://', 'https://');
+};
